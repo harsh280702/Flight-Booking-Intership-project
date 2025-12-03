4098b4f317d492146740aca98171c7dab35d3cd9
harsh <149947206+harsh280702@users.noreply.github.com>
2025-12-03 19:19:29 +0530

    add data

--- Fb-Booking-Application/src/main/java/com/fbcode/Fb/Booking/Application/controller/user/UserController.java
+++ Fb-Booking-Application/src/main/java/com/fbcode/Fb/Booking/Application/controller/user/UserController.java
@@ -172,6 +172,7 @@ public class UserController {
         UserResponseDTO userResponseDTO =  new UserResponseDTO();
         return userServices.getUserById(id)
                 .map(userResponseDTO1 ->   ResponseEntity.status( userResponseDTO.getStatusCode()).body(userResponseDTO.getUser()))
+
                 .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)));
     }
 
