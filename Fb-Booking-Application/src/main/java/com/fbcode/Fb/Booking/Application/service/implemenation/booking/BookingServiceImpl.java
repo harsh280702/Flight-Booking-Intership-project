e95c80abe40ca8f305ba3fde714b3179b188fbbb
harsh <149947206+harsh280702@users.noreply.github.com>
2025-12-03 09:36:54 +0530

    add data

--- Fb-Booking-Application/src/main/java/com/fbcode/Fb/Booking/Application/service/implemenation/booking/BookingServiceImpl.java
+++ Fb-Booking-Application/src/main/java/com/fbcode/Fb/Booking/Application/service/implemenation/booking/BookingServiceImpl.java
@@ -2,10 +2,11 @@ package com.fbcode.Fb.Booking.Application.service.implemenation.booking;
 
 import com.fbcode.Fb.Booking.Application.repositry.flight.FlightRepository;
 import com.fbcode.Fb.Booking.Application.repositry.user.UserRepositry;
+import com.fbcode.Fb.Booking.Application.service.InterFace.booking.IBooking;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookingServiceImpl {
+public class BookingServiceImpl implements IBooking {
 
     private FlightRepository flightRepository;
     private UserRepositry userRepositry;
