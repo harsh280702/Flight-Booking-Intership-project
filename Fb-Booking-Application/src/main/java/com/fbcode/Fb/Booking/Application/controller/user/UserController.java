906acf060bd3b0a5ce39d51c5e002f8330a3c2f7
harsh <149947206+harsh280702@users.noreply.github.com>
2025-12-03 19:18:39 +0530

    add data

--- Fb-Booking-Application/src/main/java/com/fbcode/Fb/Booking/Application/controller/user/UserController.java
+++ Fb-Booking-Application/src/main/java/com/fbcode/Fb/Booking/Application/controller/user/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -95,7 +96,7 @@ public class UserController {
         UserResponseDTO   userResponseDTO =  new UserResponseDTO();
         return userServices.updateUser(id,userEntity)
                 .map(userResponseDTO1 ->   ResponseEntity.status( userResponseDTO.getStatusCode()).body(userResponseDTO.getUser()))
-                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).body(null)));
+                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)));
 
 
     }
@@ -106,7 +107,7 @@ public class UserController {
         UserResponseDTO userResponseDTO =  new UserResponseDTO();
         return userServices.deleteUser(id)
                 .map(userResponseDTO1 ->   ResponseEntity.status( userResponseDTO.getStatusCode()).body(userResponseDTO.getMessage()))
-                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.NO_CONTENT).body(userResponseDTO.getMessage())));
+                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(userResponseDTO.getMessage())));
     }
 
 
@@ -117,7 +118,7 @@ public class UserController {
         UserResponseDTO userResponseDTO =  new UserResponseDTO();
         return userServices.Logout(token)
                 .map(userResponseDTO1 ->   ResponseEntity.status( 200).body("Logout Successfully"))
-                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null)));
+                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)));
 
 
     }
@@ -128,20 +129,51 @@ public class UserController {
         UserResponseDTO userResponseDTO =  new UserResponseDTO();
         return userServices.getUserByEmail(email)
                 .map(userResponseDTO1 ->   ResponseEntity.status( userResponseDTO.getStatusCode()).body(userResponseDTO.getUser()))
-                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null)));
+                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)));
     }
 
 
     @PreAuthorize("hasrole('USER','ADMIN')")
-    @GetMapping("")
-    public Mono<ResponseEntity<UserEntityDto>> getUserByUserName(@PathVariable("email") String username){
+    @GetMapping("/user/{username}")
+    public Mono<ResponseEntity<UserEntityDto>> getUserByUserName(@PathVariable("username") String username){
         UserResponseDTO userResponseDTO =  new UserResponseDTO();
         return userServices.getUserByUsername(username)
                 .map(userResponseDTO1 ->   ResponseEntity.status( userResponseDTO.getStatusCode()).body(userResponseDTO.getUser()))
-                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null)));
+                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)));
     }
 
 
+           @PreAuthorize("hasRole('ADMIN')")
+           @GetMapping("/all-users")
+          public Flux<ResponseEntity<UserEntityDto>> Users(){
+               UserResponseDTO  userResponseDTO =  new UserResponseDTO();
+               return userServices.getAllUsers()
+                       .map(user -> ResponseEntity
+                               .status(HttpStatus.OK)
+                               .body(userResponseDTO.getUser()))
+                       .onErrorResume(error ->
+                               Flux.just(ResponseEntity
+                                       .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                       .body(null)));}
+
+    @PreAuthorize("hasrole('USER','ADMIN')")
+    @GetMapping("/get-userBy-Phone/{phone}")
+    public Mono<ResponseEntity<UserEntityDto>> getUserByPhone(@PathVariable("phone") String phone){
+        UserResponseDTO userResponseDTO =  new UserResponseDTO();
+        return userServices.getUserByPhone(phone)
+                .map(userResponseDTO1 ->   ResponseEntity.status( userResponseDTO.getStatusCode()).body(userResponseDTO.getUser()))
+                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)));
+    }
+
+
+    @PreAuthorize("hasrole('USER','ADMIN')")
+    @GetMapping("/get-user-by-Id/{id}")
+    public Mono<ResponseEntity<UserEntityDto>> getUserById(@PathVariable("id") long id ){
+        UserResponseDTO userResponseDTO =  new UserResponseDTO();
+        return userServices.getUserById(id)
+                .map(userResponseDTO1 ->   ResponseEntity.status( userResponseDTO.getStatusCode()).body(userResponseDTO.getUser()))
+                .onErrorResume(throwable -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)));
+    }
 
 
 
